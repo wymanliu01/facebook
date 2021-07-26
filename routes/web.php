b42c660046d057c00be1d34a544b18a8c42c6e47
wymanliu1130 <wyman@digisalad.cool>
2021-07-27 00:15:46 +0800

    init

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/facebook/redirect', function () {
+    return Socialite::driver('facebook')->redirect();
+});
+
+Route::get('/facebook/callback', function () {
+    $user = Socialite::driver('facebook')->user();
 });
